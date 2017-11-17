fc98dc9beea0f17c36589e7a6864fe237b46061b
lijinfeng <lijinfeng520520@live.com>
2017-11-17 15:13:19 +0800

    ss

---
+++ MyMVCWebApi/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyApi.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "我的API测试";
+
+            return View();
+        }
+        public string Get(string a, string b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ MyMVCWebApi/Models/ApiModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyApi.Controllers
+{
+    public class ApiResponseMessage : iApiMessage
+    {
+        public string Date
+        {
+            get
+            {
+                return DateTime.Now.ToString("yyyy/MM/dd ddd hh:mm:ss");
+            }
+        }
+        public string User { get; set; }
+        public bool Success { get; set; }
+        public string ContentType { get; set; }
+        public string Message { get; set; }
+    }
+    public class ApiRequestMessage : iApiMessage
+    {
+        public string ContentType { get; set; }
+        public string Message { get; set; }
+
+        public string Date { get; set; }
+        public string Chanel { get; set; }
+        public bool Success { get; set; }
+        public string User { get; set; }
+    }
+    interface iApiMessage
+    {
+        string ContentType { get; set; }
+        string Message { get; set; }
+        string Date { get; }
+        bool Success { get; set; }
+    }
+}
